a25e658b7e54ce5d07fb5bdc5a0cd298e30c3416
erc-uchida <t-uchida@e-reverse.com>
2017-11-19 17:50:33 +0900

    コンポーネントイベント実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent
+  implements OnInit, OnChanges, OnDestroy, AfterViewInit, AfterViewChecked, AfterContentInit, AfterContentChecked, DoCheck {
+
+  title = 'Angular component lifecycle.';
+
+  constructor() {
+    this.log('constructor');
+  }
+
+  ngOnChanges() {
+    this.log('ngOnChanges');
+  }
+
+  ngDoCheck() {
+    this.log('ngDoCheck');
+  }
+
+  ngOnInit() {
+    this.log('ngOnInit');
+  }
+
+  ngOnDestroy() {
+    this.log('ngOnDestroy');
+  }
+
+  ngAfterContentInit() {
+    this.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    this.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    this.log('ngAfterViewChecked');
+  }
+
+  log(msg) {
+    console.log(`%c${msg}`, 'color: red');
+  }
 }
